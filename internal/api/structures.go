2df39a4efb1414d003743545b41f0676cd5e4700
Adil <madil6583@gmail.com>
2023-12-26 17:15:51 +0600

    structures for api retrieval created, internal/api/structures.go

---
+++ internal/api/structures.go
@@ -0,0 +1,48 @@
+package api
+
+type ArtistsAPI struct {
+	Artists   string `json:"artists"`   //api URL
+	Locations string `json:"locations"` //api URL
+	Dates     string `json:"dates"`     //api URL
+	Relation  string `json:"relation"`  //api URL
+}
+
+type Artist struct {
+	Id              int      `json:"id"`
+	Image           string   `json:"image"`
+	Name            string   `json:"name"`
+	Members         []string `json:"members"`
+	CreationDate    int      `json:"creationDate"`
+	FirstAlbum      string   `json:"firstAlbum"`
+	LocationsAPI    string   `json:"locations"`    //api URL
+	ConcertDatesAPI string   `json:"concertDates"` //api URL
+	RelationsAPI    string   `json:"realtions"`    //api URL
+}
+
+type ArtistsLocation struct {
+	Id        int      `json:"id"`
+	Locations []string `json:"locations"`
+	DatesAPI  string   `json:"dates"` //api URL
+}
+
+type ArtistsConcertDates struct {
+	Id    int      `json:"id"`
+	Dates []string `json:"dates"`
+}
+
+type ArtistsRealtion struct {
+	Id            int                 `json:"id"`
+	DatesLocation map[string][]string `json:"datesLocations"`
+}
+
+type ArtistFullData struct {
+	Id           int                 `json:"id"`
+	Image        string              `json:"image"`
+	Name         string              `json:"name"`
+	Members      []string            `json:"members"`
+	CreationDate int                 `json:"creationDate"`
+	FirstAlbum   string              `json:"firstAlbum"`
+	Locations    ArtistsLocation     `json:"artistsLocations"`
+	ConcertDates ArtistsConcertDates `json:"artistsConcertDates"`
+	Relations    ArtistsRealtion     `json:"artistsRelations"`
+}
